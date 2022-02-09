b9967778becbe4aa36f77ad8f4118faa149f9e1d
Harouna Tiambou Abdoul Wahabou <harounatiambou.pro@gmail.com>
2022-02-09 17:49:04 +0100

    Pages & Basic routing initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Routes, Link, Route } from 'react-router-dom'
+
+//Pages importation
+import { About, Cart, Categories, ContactUs, Error, Home, Login, Politicy, Registration } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <h1>Header</h1>
+      <Routes>
+        <Route path='/' element= {<Home />} />
+        <Route path='/login' element= {<Login />} />
+        <Route path='/registration' element= {<Registration />} />
+        <Route path='/about' element= {<About />} />
+        <Route path='/politicy' element= {<Politicy />} />
+        <Route path='/cart' element= {<Cart />} />
+        <Route path='/contactus' element= {<ContactUs />} />
+        <Route path='*' element= {<Error />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/pages/Politicy&Terms/Politicy.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Politicy = () => {
+  return (
+    <div>Politicy</div>
+  )
+}
+
+export default Politicy
